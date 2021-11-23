d25f989f07baed73260c22d745235349464fc096
Heitor Polidoro <14806300+heitorpolidoro@users.noreply.github.com>
2021-11-23 18:31:22 -0300

    automerge-1828 (#7)
    * automerge-1828
    
    * automerge-1828-2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,19 @@ set -e
 echo "::group::GitHub authentication"
   echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
 echo "::endgroup::"
-env
-echo "::group::Creating PR"
+
+echo "::group::Creating PullR"
   gh pr create --title "$GITHUB_REF_NAME" --body "PR Automated created" || echo ""
 echo "::endgroup::"
+
+if [[ "$INPUT_AUTOMERGE" == "true" ]]
+then
+  if [[ "$GITHUB_REPOSITORY_OWNER" != "${GITHUB_ACTOR}" ]]
+  then
+    echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
+  else
+    echo "::group::Setting to Automerge"
+      gh pr merge --auto --squash --delete-branch
+    echo "::endgroup::"
+  fi
+fi
