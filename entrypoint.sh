8a29c5618c25fbf3328f0b1ff16a844860e64533
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-24 14:16:30 -0300

    remove gh auth login

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@ BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
 set -e
-echo "::group::GitHub authentication"
-  echo "$GITHUB_TOKEN" | gh auth login --with-token
-echo "::endgroup::"
-
 echo "::group::Creating Pull Request"
   gh pr create --title "$GITHUB_REF_NAME" --body "PR Automated created" || echo ""
 echo "::endgroup::"
