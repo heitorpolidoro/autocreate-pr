71927945559bab4f66c26523fa4af3689d747838
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-24 14:54:02 -0300

    auth with actor

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
 set -e
+echo "::group::GitHub authentication"
+  eval echo "\$INPUT_AUTOCREATE_PR_$GITHUB_ACTOR"
+  eval echo "\$INPUT_AUTOCREATE_PR_$GITHUB_ACTOR" | gh auth login --with-token
+  gh auth status
+echo "::endgroup::"
+
 echo "::group::Creating Pull Request"
   gh pr create --title "$GITHUB_REF_NAME" --body "PR Automated created" || echo ""
 echo "::endgroup::"
