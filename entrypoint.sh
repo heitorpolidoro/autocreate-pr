226f8f698935b7f548d5e35e3b77400939ad4efc
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-24 16:46:17 -0300

    validate allowed user 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@ BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
 set -e
-echo "::group::GitHub authentication"
-  echo "$GITHUB_ACTOR"
-  eval echo "\$$GITHUB_ACTOR"
-  eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
-  gh auth status
-echo "::endgroup::"
+token=$(eval echo "\$$GITHUB_ACTOR")
+if [[ -z "$token" ]]
+then
+  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
+else
+  echo "::group::GitHub authentication"
+    eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
+  echo "::endgroup::"
 
-echo "::group::Creating Pull Request"
-  gh pr create --title "$GITHUB_REF_NAME" --body "PR Automated created" || echo ""
-echo "::endgroup::"
+  echo "::group::Creating Pull Request"
+    gh pr create --title "$GITHUB_REF_NAME" --body "PR Automated created" || echo ""
+  echo "::endgroup::"
+fi
