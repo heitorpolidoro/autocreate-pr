70169459abf6c4ab2f7a53d0b661086d1fd6d936
Heitor Polidoro <14806300+heitorpolidoro@users.noreply.github.com>
2021-11-25 07:59:23 -0300

    auto-release #14 #15 (#16)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ set -e
 token=$(eval echo "\$$GITHUB_ACTOR")
 if [[ -z "$token" ]]
 then
-  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
+  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto create Pull Request"
 else
   echo "::group::GitHub authentication"
     eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
+    gh auth status
   echo "::endgroup::"
 
   echo "::group::Creating Pull Request"
