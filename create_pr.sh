5c6c8c7a74b26fb484c0dd1ecee06e41bb76cebe
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-02-28 19:47:38 -0300

    format

--- create_pr.sh
+++ create_pr.sh
@@ -2,26 +2,21 @@
 BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
-if [[ $# < 1 ]]
-then
-  echo sh-checker
-fi
 echo "::group::Workaround"
 git config --global --add safe.directory /github/workspace
 echo "::endgroup::"
 
 set -e
 token=$(eval echo "\$$GITHUB_ACTOR")
-if [[ -z "$token" ]]
-then
-  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto create Pull Request"
+if [[ -z "$token" ]]; then
+	echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto create Pull Request"
 else
-  echo "::group::GitHub authentication ($GITHUB_ACTOR)"
-    eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
-    gh auth status
-  echo "::endgroup::"
+	echo "::group::GitHub authentication ($GITHUB_ACTOR)"
+	eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
+	gh auth status
+	echo "::endgroup::"
 
-  echo "::group::Creating Pull Request"
-    gh pr create --title "$GITHUB_REF_NAME" --body "PR automatically created" || echo ""
-  echo "::endgroup::"
+	echo "::group::Creating Pull Request"
+	gh pr create --title "$GITHUB_REF_NAME" --body "PR automatically created" || echo ""
+	echo "::endgroup::"
 fi
