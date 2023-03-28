b6c6f7722a36bf826dc02ac6286d2899e0afcda6
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-03-28 15:07:38 -0300

    Fix missing token error

--- create_pr.sh
+++ create_pr.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-BOLD='\x1b[1m'
-NORMAL="\x1b[0m"
-
 echo "::group::Workaround"
 git config --global --add safe.directory /github/workspace
 echo "::endgroup::"
 
 set -e
 token=$(eval echo "\$$GITHUB_ACTOR")
-if [[ "$token" == "$" ]]; then
-	echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto create Pull Request"
+if [[ "$token" == "$" || "$token" == "" ]]; then
+	echo -e "User '$GITHUB_ACTOR' is not allowed to auto create Pull Request"
 else
 	echo "::group::GitHub authentication ($GITHUB_ACTOR)"
 	eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
