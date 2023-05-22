1295d3aa6f9f3cbe8a9a47f495283a7c67f823cd
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-05-22 10:57:00 -0300

    space

--- .space.kts
+++ .space.kts
@@ -8,17 +8,6 @@ job("Hello World!") {
     container(displayName = "Say Hello", image = "hello-world")
 }
 
-container("amazoncorretto:17-alpine") {
-    kotlinScript { api ->
-        api.space().projects.automation.deployments.start(
-            project = api.projectIdentifier(),
-            targetIdentifier = TargetIdentifier.Key("github-marketplace"),
-            version = "1.0.0",
-            // automatically update deployment status based on a status of a job
-            syncWithAutomationJob = true
-        )
-    }
-}
 
 job("Example shell script") {
     container(displayName = "Say Hello", image = "ubuntu") {
