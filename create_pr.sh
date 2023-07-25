93d6ad39b65428983219581a9567ed369ccb2cd7
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-07-25 17:59:56 -0300

    env debug 4

--- create_pr.sh
+++ create_pr.sh
@@ -2,27 +2,26 @@
 #echo "::group::Workaround"
 #git config --global --add safe.directory /github/workspace
 #echo "::endgroup::"
-env | sort
-#set -e
-#token=$(eval echo "\$$GITHUB_ACTOR")
-#if [[ "$token" == "$" || "$token" == "" ]]; then
-#	echo -e "User '$GITHUB_ACTOR' is not allowed to auto create Pull Request"
-#else
-#	echo "::group::GitHub authentication ($GITHUB_ACTOR)"
-#	eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
-#	gh auth status
-#	echo "::endgroup::"
-#
-#	echo "::group::Creating Pull Request"
-#	gh pr create --title "$GITHUB_REF_NAME" --body "PR automatically created" || echo ""
-#	echo "::endgroup::"
-#
-#echo "automerge=$INPUT_AUTOMERGE"
-#env
-#	if [[ "$INPUT_AUTOMERGE" == "true" ]]; then
-#		echo "::group::Configuring to auto merge"
-#		gh pr merge --auto --squash
-#		echo "::endgroup::"
-#	fi
-#
-#fi
+
+set -e
+token=$(eval echo "\$$GITHUB_ACTOR")
+if [[ "$token" == "$" || "$token" == "" ]]; then
+	echo -e "User '$GITHUB_ACTOR' is not allowed to auto create Pull Request"
+else
+	echo "::group::GitHub authentication ($GITHUB_ACTOR)"
+	eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
+	gh auth status
+	echo "::endgroup::"
+
+	echo "::group::Creating Pull Request"
+	gh pr create --title "$GITHUB_REF_NAME" --body "PR automatically created" || echo ""
+	echo "::endgroup::"
+
+  echo "automerge=$INPUT_AUTOMERGE"
+	if [[ "$INPUT_AUTOMERGE" == "true" ]]; then
+		echo "::group::Configuring to auto merge"
+		gh pr merge --auto --squash
+		echo "::endgroup::"
+	fi
+
+fi
