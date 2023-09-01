a96e7b65eff03da374ac8fe9b3951169f62dacf1
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-09-01 11:43:01 -0300

    style(create_pr.sh): linter

--- create_pr.sh
+++ create_pr.sh
@@ -5,11 +5,11 @@ token=$(eval echo "\$$GITHUB_ACTOR")
 if [[ "$GITHUB_TOKEN" == "" && ("$token" == "$" || "$token" == "") ]]; then
 	echo -e "User '$GITHUB_ACTOR' is not allowed to auto create Pull Request"
 else
-  if [[ "$GITHUB_TOKEN" == "" ]]; then
-	  echo "::group::GitHub authentication ($GITHUB_ACTOR)"
-	  echo "$token" | gh auth login --with-token
-	  gh auth status
-	  echo "::endgroup::"
+	if [[ "$GITHUB_TOKEN" == "" ]]; then
+		echo "::group::GitHub authentication ($GITHUB_ACTOR)"
+		echo "$token" | gh auth login --with-token
+		gh auth status
+		echo "::endgroup::"
 	fi
 
 	echo "::group::Creating Pull Request"
