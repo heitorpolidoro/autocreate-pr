145758e0b8dfe46bd8b153bc1bffb3bf9ed0ca17
Heitor Polidoro <14806300+heitorpolidoro@users.noreply.github.com>
2023-09-01 11:45:22 -0300

    Enable to all (#54)
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * Enable to all2 (#53)
    
    * feat(permission): enable to all
    
    * enable-to-all3 (#52)
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * feat(permission): enable to all
    
    * style(create_pr.sh): linter
    
    ---------
    
    Co-authored-by: Heitor Polidoro <heitor.polidoro@gmail.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- create_pr.sh
+++ create_pr.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
+
 token=$(eval echo "\$$GITHUB_ACTOR")
-if [[ "$token" == "$" || "$token" == "" ]]; then
+if [[ "$GITHUB_TOKEN" == "" && ("$token" == "$" || "$token" == "") ]]; then
 	echo -e "User '$GITHUB_ACTOR' is not allowed to auto create Pull Request"
 else
-	echo "::group::GitHub authentication ($GITHUB_ACTOR)"
-	eval echo "\$$GITHUB_ACTOR" | gh auth login --with-token
-	gh auth status
-	echo "::endgroup::"
+	if [[ "$GITHUB_TOKEN" == "" ]]; then
+		echo "::group::GitHub authentication ($GITHUB_ACTOR)"
+		echo "$token" | gh auth login --with-token
+		gh auth status
+		echo "::endgroup::"
+	fi
 
 	echo "::group::Creating Pull Request"
 	gh pr create --title "$GITHUB_REF_NAME" --body "PR automatically created" || echo ""
